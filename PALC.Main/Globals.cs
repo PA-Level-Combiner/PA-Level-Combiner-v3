da14c0e2ebb35680b33c556c150938d48e3caf9f
TNTzx <azelynnrituapersonality@gmail.com>
2024-02-11 22:02:47 +0800

    changed github link

--- PALC.Main/Globals.cs
+++ PALC.Main/Globals.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace PALC.Main;
@@ -9,4 +10,11 @@ public static class Globals
     public static readonly string PALCVersion =
         FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion?.Split("+")[0]
         ?? "Unknown version?? wtf";
+
+    public static readonly string githubLink = @"https://github.com/PA-Level-Combiner/PA-Level-Combiner-v3";
+    public static readonly string githubIssuesLink = $"{githubLink}/issues";
+    public static readonly string githubReleasesLink = "${githubLink}/releases";
+    public static readonly long githubID = 718512825;
+
+    public static readonly string logsPath = $"{AppDomain.CurrentDomain.BaseDirectory}logs";
 }
