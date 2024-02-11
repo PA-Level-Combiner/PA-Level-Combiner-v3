5bbfb7eddf705e1c704d95cbb668ea6bfdf103ca
TNTzx <azelynnrituapersonality@gmail.com>
2024-02-11 22:46:58 +0800

    updated versions to more stable ones

--- PALC.Main/ViewModels/Splash/Inner/AboutVM.cs
+++ PALC.Main/ViewModels/Splash/Inner/AboutVM.cs
@@ -1,4 +1,7 @@
-﻿namespace PALC.Main.ViewModels.Splash.Inner;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+
+namespace PALC.Main.ViewModels.Splash.Inner;
 
 
 public partial class AboutVM : ViewModelBase
@@ -7,4 +10,11 @@ public partial class AboutVM : ViewModelBase
 
     public static string GithubLink => Globals.githubLink;
     public static string GithubIssuesLink => Globals.githubIssuesLink;
+
+
+    [RelayCommand]
+    public static void Crash()
+    {
+        throw new Exception("Manual crash!! WTF!! Why!!");
+    }
 }
