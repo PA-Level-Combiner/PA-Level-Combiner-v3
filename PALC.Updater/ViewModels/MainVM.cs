a57a9cabdb2bb507cb26a1a3817818b5bb6fe1e6
TNTzx <azelynnrituapersonality@gmail.com>
2024-02-11 21:52:01 +0800

    move PALC.Common to new repo

--- PALC.Updater/ViewModels/MainVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PALC.Updater.ViewModels;
-
-
-
-public partial class MainVM : ViewModelBase
-{
-    public static string Greeting => "Welcome to Avalonia!";
-}
--- PALC.Updater/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace PALC.Updater.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
