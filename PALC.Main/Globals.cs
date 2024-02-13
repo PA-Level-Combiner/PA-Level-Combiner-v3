3a4098b6e57330f9b9de0f40010140ebffc8e4c3
TNTzx <azelynnrituapersonality@gmail.com>
2024-02-13 18:51:01 +0800

    globals

--- PALC.Main/Globals.cs
+++ PALC.Main/Globals.cs
@@ -7,14 +7,9 @@ namespace PALC.Main;
 
 public static class Globals
 {
-    public static readonly string PALCVersion =
-        FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion?.Split("+")[0]
-        ?? "Unknown version?? wtf";
-
     public static readonly string githubLink = @"https://github.com/PA-Level-Combiner/PA-Level-Combiner-v3";
     public static readonly string githubIssuesLink = $"{githubLink}/issues";
     public static readonly string githubReleasesLink = "${githubLink}/releases";
-    public static readonly long githubID = 718512825;
 
     public static readonly string logsPath = $"{AppDomain.CurrentDomain.BaseDirectory}logs";
 }
